87722a57f934509bb99fe6e0d7f7ff8e145e67e9
Dana Harrington <dana.harrington@nurun.com>
2014-07-26 12:06:24 -0400

    Flesh out the parser spec with some tests

--- src/main/scala/com/nurun/icfp_2014/parser/Expr.scala
+++ src/main/scala/com/nurun/icfp_2014/parser/Expr.scala
@@ -4,6 +4,7 @@ package com.nurun.icfp_2014.parser
  * Created by dana.harrington on 2014-07-26.
  */
 sealed trait Expr
-case class Constant(c: Int)
-case class App(fn: String, args: Seq[Expr])
-case class Abs(args: Seq[String], body: Expr)
+case class Constant(c: Int) extends Expr
+case class Literal(v: String) extends Expr
+case class App(fn: Expr, args: Seq[Expr]) extends Expr
+case class Abs(args: Seq[String], body: Expr) extends Expr
